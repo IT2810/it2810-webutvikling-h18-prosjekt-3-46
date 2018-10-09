0baeb0151f31053971f001dd0b335d1c91315252
Magnus Schjølberg <37403247+Magssch@users.noreply.github.com>
2018-10-09 20:51:02 +0200

    Implementert React Navigation og begynt på To Do List
    Har implementert React Navigation-komponenten og begynt implementasjon av To Do-liste.
    
    #11, #5

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, AsyncStorage, Button} from 'react-native';
+import TodoList from './src/components/TodoList';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     
 
     return (
       <View style={styles.container}>
-        <Text>We are started boys</Text>
+        <Text>We ar
+            e started boys</Text>
         <Image source={require('./src/assets/smund.png')} style={{width: 300, height: 400}}/>
+          <Button
+              title="Go to ToDo"
+              onPress={() => this.props.navigation.navigate('ToDo')}
+          />
       </View>
     );
   }
 }
+
+export default createStackNavigator({
+    Home: {
+        screen: HomeScreen
+    },
+    ToDo: {
+        screen: TodoList
+    },
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -20,5 +36,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+      width: "100%"
   },
 });
