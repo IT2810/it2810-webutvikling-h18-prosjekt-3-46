c2c7e4e6950daaaaa6b63c0e122f4bcf83ce9018
Magnus Schjølberg <37403247+Magssch@users.noreply.github.com>
2018-10-16 14:56:36 +0200

    Ferdig implementert tabs
    Har implementert tab-navigasjon og funnet icons til dette #1 #2

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Image, Button} from 'react-native';
 import TodoList from './src/components/TodoList';
 import DailyProgress from './src/components/DailyProgress';
 import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
-import {Toast} from "native-base";
+import {Icon} from "native-base";
 
 class HomeScreen extends React.Component {
 
@@ -29,21 +29,79 @@ class HomeScreen extends React.Component {
     );
   }
 }
-
+/*
 export default createBottomTabNavigator({
         Home: {
             screen: HomeScreen
         },
         "Tasks": {
-            screen: createStackNavigator({TodoList: {screen: TodoList}})
+            screen: createStackNavigator({TodoList: {screen: TodoList}}),
+            navigationOptions: () => ({
+                tabBarIcon: ({tintColor}) => (
+                    <Icon
+                        name="ios-list-box"
+                        type="Ionicons"
+                        color={tintColor}
+                        size={10}
+                    />
+                )
+            }),
+            tabBarOptions: {
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }
         },
         "Daily Progress": {
             screen: createStackNavigator({DailyProgress: {screen: DailyProgress}})
         }
     },
     {
+        tabBarOptions: {
+                activeTintColor: '#F8F8F8', // active icon color
+                inactiveTintColor: '#586589',  // inactive icon color
+                style: {
+                backgroundColor: '#FFF' // TabBar background
+            }
+        }
+    },
+    {
         portraitOnlyMode: true
-    });
+    });*/
+
+export default createBottomTabNavigator(
+    {
+        Home: HomeScreen,
+        "Tasks": createStackNavigator({TodoList: {screen: TodoList}}),
+        "Daily Progress": createStackNavigator({DailyProgress: {screen: DailyProgress}})
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                let type;
+                if (routeName === 'Home') {
+                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                    type="Ionicons";
+                } else if (routeName === 'Tasks') {
+                    iconName = `ios-list${focused ? '-box' : ''}`;
+                    type="Ionicons";
+                } else if (routeName === 'Daily Progress') {
+                    iconName = `heart${focused ? 'beat' :  '-o'}`;
+                    type = "FontAwesome";
+                }
+                return <Icon type={type} name={iconName} size={25} color={tintColor} style={{marginTop: 2, marginBottom: 2}} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: '#007aff',
+            inactiveTintColor: 'gray',
+        },
+    },
+    {
+        portraitOnlyMode: true
+    }
+);
 
 const styles = StyleSheet.create({
   container: {
