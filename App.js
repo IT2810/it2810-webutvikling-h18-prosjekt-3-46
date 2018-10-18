beb178f35c3788354b6b277ab0ff1adca157ec44
AsmundH <aasmund.haugse@gmail.com>
2018-10-18 21:50:39 +0200

    Har fungerende homepage, men ikke som egen komponent

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity} from 'react-native';
 import TodoList from './src/components/TodoList';
 import Calendar from './src/components/Calendar';
 import DailyProgress from './src/components/DailyProgress';
@@ -7,18 +7,59 @@ import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
 import {Icon} from "native-base";
 
 class HomeScreen extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            quotes: [
+                "Don't judge each day by the harvest you reap but by the seeds that you plant. ",
+                "Write it on your heart that every day is the best day in the year. ",
+                "Every moment is a fresh beginning.",
+                "Everything you’ve ever wanted is on the other side of fear. ",
+                "Begin at the beginning… and go on till you come to the end: then stop.",
+                "Make each day your masterpiece.",
+                "You cannot tailor-make the situations in life but you can tailor-make the attitudes to fit those situations.",
+                "The day is what you make it! So why not make it a great one?",
+                "To be the best, you must be able to handle the worst.",
+                "Believe and act as if it were impossible to fail.",
+                "You must be the change you wish to see in the world.",
+                "The greatest discovery of all time is that a person can change his future by merely changing his attitude."
+            ],
+            displayText: "You must be the change you wish to see in the world."
+        };
+    };
 
     static navigationOptions = {
         title: 'Home'
     };
 
-    render() {
 
+    render() {
         return (
             <View style={styles.container}>
-                
+                <ImageBackground
+                    source={require('./src/assets/background.jpg')}
+                    imageStyle={{resizeMode: 'stretch'}}
+                    style={styles.image}>
+                    <TouchableOpacity onPress={this.toggleNewQuote}>
+                        <View style={{backgroundColor:'rgba(0, 0, 0, 0.65)', padding:20, borderRadius: 15, width: "90%"}}>
+                            <Text style={styles.paragraph}>
+                                {this.state.displayText}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </ImageBackground>
             </View>
         );
+        
+    }
+
+    toggleNewQuote = () => {
+        var rand = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
+        if (rand===this.state.displayText) {
+            this.toggleNewQuote
+        } else {
+            this.setState({displayText: rand});
+        }
     }
 }
 
@@ -63,12 +104,22 @@ export default createBottomTabNavigator(
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-      width: "100%"
-  },
-});
-
+    container: {
+      flex: 1,
+      alignItems: 'stretch',
+      justifyContent: 'center',
+    },
+    image: {
+      flexGrow:1,
+      height:'100%',
+      width:null,
+      alignItems: 'center',
+      justifyContent:'center',
+    },
+    paragraph: {
+      textAlign: 'center',
+      color: 'white',
+      fontSize: 30,
+      
+    },
+  });
