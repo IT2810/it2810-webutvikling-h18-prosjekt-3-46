6dcb3f388c55b43645ec33e365a805c70eddcba8
Magnus Schjølberg <37403247+Magssch@users.noreply.github.com>
2018-10-19 19:51:46 +0200

    Lagt til flere kommentarer i Kalender og DailyProgress #27

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -82,7 +82,7 @@ export default class AgendaScreen extends Component {
                                 <Text style={{marginTop: 30}}>Edit a note for this day:</Text>
                                 <View style={styles.textContainer}>
                                     <TextInput
-                                        style={styles.textInput}
+                                        style={[styles.textInput,  {height: Platform.OS === 'android' ? 300 : 150}]}
                                         multiline={true}
                                         numberOfLines={5}
                                         keyboardType = 'default'
@@ -306,7 +306,6 @@ const styles = StyleSheet.create({
         width: "90%"
     },
     textInput: {
-        height: 150,
         paddingRight: 15,
         paddingLeft: 15,
         paddingTop: 15,
